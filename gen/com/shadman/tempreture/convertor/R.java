26d89179141ee06982cac6c43ea18686f9cc7937
Salim Shadman <salimshadman2@gmail.com>
2014-05-30 10:46:56 +0530

    Revert "Source"
    This reverts commit e66587734434731eac8ba3dda9bba3b475277dc6.

--- gen/com/shadman/tempreture/convertor/R.java
+++ gen/com/shadman/tempreture/convertor/R.java
@@ -27,13 +27,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090006;
+        public static final int action_settings=0x7f090005;
         public static final int button1=0x7f090004;
-        public static final int checkedTextView1=0x7f090005;
-        public static final int editText1=0x7f090001;
+        public static final int editText1=0x7f090000;
         public static final int radio0=0x7f090002;
         public static final int radio1=0x7f090003;
-        public static final int radioGroup1=0x7f090000;
+        public static final int radioGroup1=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
